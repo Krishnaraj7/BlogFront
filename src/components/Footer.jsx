647e6db261a672dfc10684cd5596b6bc48c66822
krishnaraj <krishnarajb0942@gmail.com>
2024-02-25 21:20:30 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+
+
+const Footer = () => {
+  return (
+    <>
+
+    <div className="mt-16">
+      <p className="py-2 pb-6 text-center text-white bg-black text-sm ">All rights reserved @Blog Market 2023</p>
+    </div>
+    </>
+    
+  )
+}
+
+export default Footer
